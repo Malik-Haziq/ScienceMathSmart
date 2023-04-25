988bee288da1b61dc70f8f62245fb79a4a8c1f85
Malik-Haziq <malikhaziq153@gmail.com>
2023-04-25 20:31:21 +0500

    update changes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
+$("#myButton").venomButton({
+  phone: "+923204694069",
+  popupMessage: "Hello, how can we help you?",
+  message: "",
+  showPopup: true,
+  position: "right",
+  linkButton: false,
+  showOnIE: false,
+  headerTitle: "Welcome!",
+  headerColor: "#25d366",
+  backgroundColor: "#25d366",
+  buttonImage: '<img src="./whatsapp-click-to-chat/whatsapp.svg" />',
+  size: "50px",
+});
+
 AOS.init({
   // delay: 200, // values from 0 to 3000, with step 50ms
   duration: 1000, // values from 0 to 3000, with step 50ms
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-(
-  module.exports = {
-    content: [
-      "./components/home/index.html",
-      "./components/fee-structures/uk-fee-structure.html",
-      "./components/fee-structures/us-fee-structure.html",
-      "./components/curriculum/math.html",
-      "./components/curriculum/physics.html",
-      "./components/curriculum/science.html",
-      "./components/curriculum/uk-curriculum.html",
-      "./components/curriculum/us-curriculum.html",
-      "./components/contact-us/contact.html",
-      "./components/footer/footer.html",
-      "./components/navigation/navigation.html",
-      "./components/Trial/trial.html",
-    ],
-    theme: {
-      extend: {},
-      fontFamily: {
-        "PT-serif": ['"PT serif"', "serif"],
-      },
+
+module.exports = {
+  content: [
+    "/index.html",
+    "/uk-fee-structure.html",
+    "/us-fee-structure.html",
+    "/math.html",
+    "/physics.html",
+    "/science.html",
+    "/uk-curriculum.html",
+    "/us-curriculum.html",
+    "/contact.html",
+    "footer.html",
+    "/navigation.html",
+    "trial.html",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "PT-serif": ['"PT serif"', "serif"],
     },
-    plugins: [],
-  }
-);
+  },
+  plugins: [],
+};

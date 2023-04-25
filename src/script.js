84d3d4fca5e8a43b878baa3f3770f998f862d413
Malik-Haziq <malikhaziq153@gmail.com>
2023-04-25 19:05:13 +0500

    changes

--- src/script.js
+++ src/script.js
@@ -4,14 +4,3 @@ AOS.init({
   once: true, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
 });
-function sendEmail() {
-  Email.send({
-    Host: 'smtp.elasticemail.com',
-    Username: 'username',
-    Password: 'password',
-    To: 'them@website.com',
-    From: 'you@isp.com',
-    Subject: 'This is the subject',
-    Body: 'And this is the body',
-  }).then((message) => alert(message));
-}

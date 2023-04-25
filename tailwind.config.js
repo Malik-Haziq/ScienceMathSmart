71efcff4481b7c904ef92394909515e99b81e37e
Malik-Haziq <malikhaziq153@gmail.com>
2023-04-25 14:22:57 +0500

    update changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './components/home/index.html',
-    './components/fee-structures/uk-fee-structure.html',
-    './components/fee-structures/us-fee-structure.html',
-    './components/curriculum/math.html',
-    './components/curriculum/physics.html',
-    './components/curriculum/science.html',
-    './components/curriculum/uk-curriculum.html',
-    './components/curriculum/us-curriculum.html',
-    './components/contact-us/contact.html',
-    './components/footer/footer.html',
-    './components/navigation/navigation.html',
-    './components/Trial/trial.html',
-  ],
-  theme: {
-    extend: {},
-    fontFamily: {
-      'PT-serif': ['"PT serif"', 'serif'],
+(
+  module.exports = {
+    content: [
+      "./components/home/index.html",
+      "./components/fee-structures/uk-fee-structure.html",
+      "./components/fee-structures/us-fee-structure.html",
+      "./components/curriculum/math.html",
+      "./components/curriculum/physics.html",
+      "./components/curriculum/science.html",
+      "./components/curriculum/uk-curriculum.html",
+      "./components/curriculum/us-curriculum.html",
+      "./components/contact-us/contact.html",
+      "./components/footer/footer.html",
+      "./components/navigation/navigation.html",
+      "./components/Trial/trial.html",
+    ],
+    theme: {
+      extend: {},
+      fontFamily: {
+        "PT-serif": ['"PT serif"', "serif"],
+      },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  }
+);
